5bd7ca1fcd6f6b9938041b92867a804074e15810
xsenia <belkinasan@yandex.ru>
2019-12-04 01:42:11 +0500

    stickyMenu

--- script/script.js
+++ script/script.js
@@ -348,6 +348,51 @@ window.addEventListener('DOMContentLoaded', function() {
         }]
     });
     carousel.init();
+
+
+    //stickyMenu
+
+    const stickyMenu = () => {
+        const windowWidth = document.documentElement.clientWidth;
+        const topMenu = document.querySelector('.top-menu');
+        const topMenuPositionTop = topMenu.getBoundingClientRect().top + window.pageYOffset;
+        const unFixedMenu = () => {
+            if (topMenu.classList.contains('sticky')) {
+                topMenu.classList.remove('sticky');
+            }
+        } 
+        const fixedMenu = () => {
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            if (scrollTop >= topMenuPositionTop) {
+                topMenu.classList.add('sticky');
+            } else {
+                topMenu.classList.remove('sticky');
+            }
+        };
+
+        if (windowWidth < 768) {            
+            window.addEventListener('scroll', function() {
+                fixedMenu();
+            });
+        }        
+
+        window.addEventListener('resize', function() {
+            let windowWidth = document.documentElement.clientWidth;
+            if (windowWidth >= 768) {
+                unFixedMenu();
+                window.addEventListener('scroll', function() {
+                    unFixedMenu();
+                });
+            } else if (windowWidth < 768) {
+                fixedMenu();
+                window.addEventListener('scroll', function() {
+                    fixedMenu();
+                });
+            }
+        });
+    };
+
+    stickyMenu();
    
 });
 
